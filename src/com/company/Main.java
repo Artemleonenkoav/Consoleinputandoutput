cbc49a1830fde80adf14a6ecc5c8cffefe67e59c
Artemleonenkoav <leonenkoavfin@gmail.com>
2021-08-03 14:30:45 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x=5;
+        int y=6;
+        System.out.println("x=" + x + "; y=" + y);
+    }
+}
